507a1121d9d41c11f5c0ef7b46f4ce7cc9707561
Denis Díaz <150247992+denis9diaz@users.noreply.github.com>
2024-05-22 10:28:39 +0000

    modificado menus.js y galeria.js con sus .css

--- src/front/js/pages/Galeria.js
+++ src/front/js/pages/Galeria.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Galeria.css";
 import Navbar from "./Navbar";
 import fondo from "../../img/fondo-info.png";
 
+import img1 from "../../img/entrantes.png";
+import img2 from "../../img/marisco.png";
+import img3 from "../../img/arroces.png";
+
 const Galeria = () => {
+    const [activeSection, setActiveSection] = useState("platos");
+
+    const renderImages = () => (
+        <>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img1} className="img-fluid" alt="Entrantes" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img2} className="img-fluid" alt="Marisco" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img3} className="img-fluid" alt="Arroces" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img1} className="img-fluid" alt="Entrantes" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img2} className="img-fluid" alt="Marisco" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img3} className="img-fluid" alt="Arroces" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img1} className="img-fluid" alt="Entrantes" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img2} className="img-fluid" alt="Marisco" /></div>
+            <div className="col-md-4 col-6 mb-4 img-container"><img src={img3} className="img-fluid" alt="Arroces" /></div>
+        </>
+    );
+
     return (
         <div className="galeria-div" style={{ backgroundImage: `url(${fondo})` }}>
             <Navbar />
-            <div>Platos</div>
-            <div>El Restaurante</div>
-            <div>Nosotros</div>
+            <div className="sections d-flex justify-content-around my-4">
+                <div 
+                    className={`section ${activeSection === "platos" ? "active" : ""}`} 
+                    onClick={() => setActiveSection("platos")}
+                >
+                    Platos
+                </div>
+                <div 
+                    className={`section ${activeSection === "restaurante" ? "active" : ""}`} 
+                    onClick={() => setActiveSection("restaurante")}
+                >
+                    El Restaurante
+                </div>
+                <div 
+                    className={`section ${activeSection === "nosotros" ? "active" : ""}`} 
+                    onClick={() => setActiveSection("nosotros")}
+                >
+                    Nosotros
+                </div>
+            </div>
+            <div className="content container">
+                <div className="row">
+                    {activeSection === "platos" && renderImages()}
+                    {activeSection === "restaurante" && renderImages()}
+                    {activeSection === "nosotros" && renderImages()}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Galeria
+export default Galeria;
--- src/front/js/pages/Menus.js
+++ src/front/js/pages/Menus.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Menus.css";
 import Navbar from "./Navbar";
 import fondo from "../../img/fondo-info.png";
 
 const Menus = () => {
+    const [activeSection, setActiveSection] = useState("menu1");
+
     return (
         <div className="menus-div" style={{ backgroundImage: `url(${fondo})` }}>
             <Navbar />
-            <div>Menu 1</div>
-            <div>Menu 2</div>
+            <div className="sections d-flex justify-content-center my-4">
+                <div 
+                    className={`section ${activeSection === "menu1" ? "active" : ""}`} 
+                    onClick={() => setActiveSection("menu1")}
+                >
+                    Menu 1
+                </div>
+                <div 
+                    className={`section ${activeSection === "menu2" ? "active" : ""}`} 
+                    onClick={() => setActiveSection("menu2")}
+                >
+                    Menu 2
+                </div>
+            </div>
+            <div className="content container">
+                {activeSection === "menu1" && (
+                    <div>
+                        {/* Contenido del Menu 1 */}
+                        <h2>Contenido del Menu 1</h2>
+                    </div>
+                )}
+                {activeSection === "menu2" && (
+                    <div>
+                        {/* Contenido del Menu 2 */}
+                        <h2>Contenido del Menu 2</h2>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
 
-export default Menus
+export default Menus;
